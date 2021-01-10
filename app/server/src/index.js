2a136bba61430a64ea9775a3998016b439f3b8c0
Dhinesh_KS <dhinesh.ks@psiog.com>
2021-01-10 21:57:41 +0530

    Express and mongoose base setup

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,1 +1,12 @@
-console.log("object")
+require("./db/mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = 4000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Server is up and running on ${port}`);
+});
