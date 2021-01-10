84999956124dc19a7fdd76a2b185599f704d8226
Dhinesh KS <dhinaksd@gmail.com>
2021-01-10 22:51:14 +0530

    Edit Post Route

--- app/server/src/routes/postMessage.js
+++ app/server/src/routes/postMessage.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const { PostMessage } = require("../models/postMessage");
 
-router.get("/posts", async (req, res) => {
+router.get("/getPosts", async (req, res) => {
   try {
     const posts = await PostMessage.find({});
     res.send(posts);
@@ -17,6 +17,30 @@ router.post("/createpost", async (req, res) => {
   try {
     await newPost.save();
     res.status(201).send(newPost);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.put("/posts/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["message"];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const editedPost = await PostMessage.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!editedPost) {
+      return res.status(404).send();
+    }
+
+    res.send(editedPost);
   } catch (e) {
     res.status(400).send(e);
   }
